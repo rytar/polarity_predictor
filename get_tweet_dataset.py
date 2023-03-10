079aef1c33b8f7985b1c1b5929f0331b578c3983
rytaryu <53173674+rytar@users.noreply.github.com>
2023-03-11 01:46:06 +0900

    yappa yameta

--- get_tweet_dataset.py
+++ get_tweet_dataset.py
@@ -11,16 +11,9 @@ for tweet in data:
     text = tweet["text"]
     score = tweet["label"][1] - tweet["label"][2]
 
-    if text == '': continue
+    if text == '' or score == 0: continue
 
-    if score > 0:
-        polarity = 1
-    elif score < 0:
-        polarity = 0
-    else:
-        polarity = 0.5
-
-    outputs.append([text, polarity])
+    outputs.append([text, 1 if score > 0 else 0])
 
 df = pd.DataFrame(outputs, columns=columns)
 df.to_csv("./tweet_dataset.csv", index=False)
